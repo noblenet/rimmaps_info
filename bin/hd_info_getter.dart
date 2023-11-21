26057af790d206f98a6944cff2580f532e71f9c5
Kim Nordskov Adelhardt <kna@pdc-argos.com>
2023-11-21 17:30:13 +0100

    Første commit med Dart CLI-program

---
+++ bin/hd_info_getter.dart
@@ -0,0 +1,5 @@
+import 'package:hd_info_getter/hd_info_getter.dart' as hd_info_getter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hd_info_getter.calculate()}!');
+}
---
+++ test/hd_info_getter_test.dart
@@ -0,0 +1,8 @@
+import 'package:hd_info_getter/hd_info_getter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
