0494cca1fe12ec47aa47b7757abb8c4f893eb310
Kim Nordskov Adelhardt <kna@pdc-argos.com>
2023-11-24 05:43:37 +0100

    in some order. Can run. Cant see any result

--- bin/hd_info_getter.dart
+++ bin/hd_info_getter.dart
@@ -1,10 +1,60 @@
+import 'dart:io';
+import 'dart:core';
+import 'dart:convert';
+import 'dart:math' as math;
 import 'package:args/args.dart';
+import 'package:path/path.dart' as p;
+import 'package:logging/logging.dart';
+import 'package:http/http.dart' as http;
+
+final Logger _logger = Logger('MyApp');
+
+void writeFilesToJson(
+    List<String> fileNamesFull, String jsonFileName, Logger logger) {
+  for (String fullFileName in fileNamesFull) {
+    File textFile = File(fullFileName);
+
+    if (!textFile.existsSync()) {
+      _logger.severe('Tekstfilen "$fullFileName" eksisterer ikke.');
+      continue; // Spring over til næste fil, hvis filen ikke eksisterer
+    }
+
+    try {
+      String context = textFile.readAsStringSync();
+      logger.fine(context);
+      // Brug path-biblioteket til at adskille filnavn og sti
+      String fileName = p.basenameWithoutExtension(fullFileName);
+      String filePath = p.dirname(fullFileName);
+
+      // Opret et map med de ønskede oplysninger
+      Map<String, dynamic> jsonMap = {
+        'fileName': fileName,
+        'path': filePath,
+        'context': context,
+      };
+
+      // Konverter map til JSON-streng
+      String jsonString = json.encode(jsonMap);
+
+      // Gem JSON i en fil med det ønskede navn
+      File jsonFile = File('$jsonFileName.json');
+      jsonFile.writeAsStringSync(jsonString);
+
+      logger.info('JSON er blevet gemt i filen: $jsonFileName.json');
+    } catch (error) {
+      logger.severe('Fejl ved behandling af filen "$fullFileName": $error');
+    }
+  }
+}
 
 void main(List<String> arguments) {
   final ArgParser argParser = ArgParser()
     ..addOption('path', abbr: 'p', mandatory: true, help: 'Sti til inputfilen')
     ..addOption('name',
-        abbr: 'n', mandatory: true, help: 'Navn på outputfilerne')
+        abbr: 'n',
+        mandatory: false,
+        defaultsTo: 'hd-output',
+        help: 'Navn på outputfilerne')
     ..addOption('output',
         abbr: 'o',
         defaultsTo: 'csv',
@@ -25,10 +75,25 @@ void main(List<String> arguments) {
       return;
     }
 
+    _logger.info('Timer starter...');
+    final stopwatch = Stopwatch()..start();
+
     final String path = args['path'];
     final String name = args['name'];
     final String outputFormat = args['output'];
     final String logLevel = args['log'];
+
+    var outputFile = File('log.log');
+
+    Logger.root.level = Level.ALL;
+    Logger.root.onRecord.listen((LogRecord rec) {
+      outputFile.writeAsStringSync(
+          "${rec.time} | ${rec.level} | ${rec.message} | ${rec.loggerName}\n",
+          mode: FileMode.append);
+    });
+
+//find the files
+    var hdFiles = findHdFiles(path);
 
     // Eksempel: Opret CSV-fil
     if (outputFormat == 'csv') {
@@ -37,7 +102,7 @@ void main(List<String> arguments) {
 
     // Eksempel: Opret JSON-fil
     if (outputFormat == 'json') {
-      createJsonFile(path, name);
+      writeFilesToJson(hdFiles, name, _logger);
     }
 
     // Logning
@@ -52,14 +117,48 @@ void main(List<String> arguments) {
   }
 }
 
+List<String> findHdFiles(String folderPath) {
+  List<String> hdFiles = [];
+
+  void searchInFolder(String folderPath) {
+    Directory(folderPath).listSync().forEach((FileSystemEntity entity) {
+      if (entity is File && entity.path.toLowerCase().endsWith('.hd')) {
+        hdFiles.add(entity.path);
+      } else if (entity is Directory) {
+        searchInFolder(entity.path);
+      }
+    });
+  }
+
+  searchInFolder(folderPath);
+  return hdFiles;
+}
+
+Future<String> callWebApi(String apiUrl) async {
+  final response = await http.get(Uri.parse(apiUrl));
+
+  if (response.statusCode == 200) {
+    // Konverter JSON-responsen til et Dart-objekt
+    return jsonDecode(response.body);
+  } else {
+    // Håndter fejl
+    throw Exception('Fejl ved kald til web-API: ${response.statusCode}');
+  }
+}
+
+Map<String, dynamic> readAndMapHdFile(String filePath) {
+  String fileContent = File(filePath).readAsStringSync();
+
+  // Implementer logik for at mappe teksten til dataobjektet
+  // ...
+
+  // Returner det dataobjekt, f.eks. et Map
+  return {'content': fileContent};
+}
+
 void createCsvFile(String path, String name) {
   // Implementer logik for oprettelse af CSV-fil
   print('Opretter CSV-fil fra sti: $path med navn: $name.csv');
-}
-
-void createJsonFile(String path, String name) {
-  // Implementer logik for oprettelse af JSON-fil
-  print('Opretter JSON-fil fra sti: $path med navn: $name.json');
 }
 
 void infoLogging() {
--- test/hd_info_getter_test.dart
+++ test/hd_info_getter_test.dart
@@ -1,7 +1,27 @@
 import 'package:test/test.dart';
+import 'package:mockito/mockito.dart';
+import '../bin/hd_info_getter.dart';
+
+// Lav en mock af File-klassen
+class MockFile extends Mock implements File {}
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('createCsvFile should print the correct message', () {
+    // Opret en instans af MockFile
+    final mockFile = MockFile();
+
+    // Kald createCsvFile-funktionen med mockFile
+    createCsvFile('mock/path', 'mock_csv', fileFactory: (path) => mockFile);
+
+    // Tjek om funktionen udskriver det forventede budskab
+    verify(mockFile.writeAsStringSync(captureAny)).called(1);
+
+    // Hent den fangete udskrift
+    String printedMessage =
+        verify(mockFile.writeAsStringSync(captureAny)).captured[0];
+
+    // Tjek om det udskrevne budskab er korrekt
+    expect(printedMessage,
+        'Opretter CSV-fil fra sti: mock/path med navn: mock_csv.csv');
   });
 }
